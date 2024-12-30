d3edce415381c2378deb007c17c3f6b58175fb50
AydosNB <nawrizbaevaydos50@gmail.com>
2024-12-30 14:15:00 +0500

    slider

--- src/components/page-comp/SwiperImage.jsx
+++ src/components/page-comp/SwiperImage.jsx
@@ -1,7 +1,36 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-const SwiperImage = () => {
+const SwiperImage = ({type}) => {
+    const imageUrlDesktop = [
+        {
+            id: 1,
+            url: "https://res.cloudinary.com/dss0yvhhr/image/upload/v1735496132/image_s_1000x1000_jen9uz.jpg"
+        },
+        {
+            id: 2,
+            url: "https://res.cloudinary.com/dss0yvhhr/image/upload/v1735487599/carpet-cleaning-service-stockcake_rcgbia.jpg"
+        },
+        {
+            id: 3,
+            url: "https://res.cloudinary.com/dss0yvhhr/image/upload/v1735496242/kover-doma-1_mo5sqe.jpg"
+        }
+    ]
+
+    const imageUrlMobile = [
+        {
+            id: 1,
+            url: "https://res.cloudinary.com/dss0yvhhr/image/upload/v1735549762/image_s_2000x1333_gt1efh.jpg"
+        },
+        {
+            id: 2,
+            url: "https://res.cloudinary.com/dss0yvhhr/image/upload/v1735549789/rug_cleaning_services_g71xm5.jpg"
+        },
+        {
+            id: 3,
+            url: "https://res.cloudinary.com/dss0yvhhr/image/upload/v1735549813/Image6_eyx25z.jpg"
+        }
+    ]
     return (
         <Swiper
             modules={[Autoplay]}
@@ -10,20 +39,22 @@ const SwiperImage = () => {
             autoplay={{ delay: 3000 }}
             speed={1500}
             loop
-            className="rounded-lg shadow-md border-[1px] border-gray-200 overflow-hidden h-full"
+            className={`rounded-lg shadow-md border-[1px] border-gray-200 overflow-hidden md:h-full`}
         >
-            <SwiperSlide>
-                <img className='max-h-[calc(100vh-110px)] w-full h-full object-cover'
-                    src="https://res.cloudinary.com/dss0yvhhr/image/upload/v1735496132/image_s_1000x1000_jen9uz.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className='max-h-[calc(100vh-110px)] w-full h-full object-cover'
-                    src="https://res.cloudinary.com/dss0yvhhr/image/upload/v1735487599/carpet-cleaning-service-stockcake_rcgbia.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className='max-h-[calc(100vh-110px)] w-full h-full object-cover'
-                    src="https://res.cloudinary.com/dss0yvhhr/image/upload/v1735496242/kover-doma-1_mo5sqe.jpg" alt="" />
-            </SwiperSlide>
+            {type === "desktop" ?
+                imageUrlDesktop.map(item => (
+                    <SwiperSlide key={item.id}>
+                        <img className='max-h-[250px] min-h-[250px] sm:max-h-[350px] sm:min-h-[350px] md:max-h-[calc(100vh-110px)] w-full h-full object-cover'
+                            src={item.url} alt="" />
+                    </SwiperSlide>
+                ))
+                :
+                imageUrlMobile.map(item => (
+                    <SwiperSlide key={item.id}>
+                        <img className='max-h-[250px] min-h-[250px] sm:max-h-[350px] sm:min-h-[350px] md:max-h-[calc(100vh-110px)] w-full h-full object-cover'
+                            src={item.url} alt="" />
+                    </SwiperSlide>
+                ))}
         </Swiper>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,17 +20,18 @@ const Home = () => {
                         </p>
                         <div data-aos="fade-up" data-aos-delay="500" className='mt-[10px]'>
                             <button className='btn-primary'>
-                                <BiPhoneCall/>
+                                <BiPhoneCall />
                                 <span>{t("contact")}</span>
                             </button>
                         </div>
                     </div>
                     <div data-aos="fade-left" data-aos-delay="500" className='flex-1 md:max-w-[40%] lg:max-w-[50%] hidden md:block'>
-                        <SwiperImage />
+                        <SwiperImage type="desktop"/>
                     </div>
-                    <div data-aos="fade-up" data-aos-delay="500" className='flex-1 block md:hidden'>
-                        <img className='h-full w-full max-h-[300px] min-h-[250px] object-cover rounded-md shadow-md border-[1px] border-gray-200'
-                            src="https://res.cloudinary.com/dss0yvhhr/image/upload/v1735487999/machine-cleaning-the-carpet-Fort-Wayne-IN_ce9ov0.jpg" alt="" />
+                    <div data-aos="fade-up" data-aos-delay="500" className='flex-1 block md:hidden max-w-full'>
+                        <SwiperImage type="mobile"/>
+                        {/* <img className='h-full w-full max-h-[300px] min-h-[250px] object-cover rounded-md shadow-md border-[1px] border-gray-200'
+                            src="https://res.cloudinary.com/dss0yvhhr/image/upload/v1735487999/machine-cleaning-the-carpet-Fort-Wayne-IN_ce9ov0.jpg" alt="" /> */}
                     </div>
                 </div>
             </Container>
